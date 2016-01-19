335a78c36f15c793577c431a7f47643cc368d2c9
Pash10g <pash10g@gmail.com>
2016-01-19 16:36:16 +0200

    Update deploy.sh

--- resources/amazon/deploy/db-create/deploy.sh
+++ resources/amazon/deploy/db-create/deploy.sh
@@ -41,7 +41,7 @@ knife cookbook upload --all || { echo "ERROR While chef upload cookbooks"; exit 
 knife upload roles ||  { echo "ERROR While chef upload roles"; exit 2; }
 cd $back_dir
 
-echo "" > /tmp/<env_name>-mongo-conf.yaml
+echo "" > /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
 
 
 for i in {1..<configsvr_number>}
@@ -51,11 +51,11 @@ do
 	deploy_vm "cpu-cores=<configsvr_cpu_core> mem=<configsvr_mem_mb>" machine_no
 	machine=$(juju status --format tabular | grep "^${machine_no} .*" | awk '{print $4}')
 	fqdn=$(juju status --format tabular | grep ${machine} | awk '{print $3}')
-	echo "configsvr${i}: " >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  replicaset : <configsvr_repl_name> " >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  config_server_port : <configsvr_port>" >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  machine: ${machine}" >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  FQDN: ${fqdn} " >> /tmp/<env_name>-mongo-conf.yaml
+	echo "configsvr${i}: " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  replicaset : <configsvr_repl_name> " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  config_server_port : <configsvr_port>" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  machine: ${machine}" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  FQDN: ${fqdn} " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
 		
 	juju deploy /root/.juju/charms/trusty/deploy-node "configsvr${i}" --series trusty --to $machine_no 
 
@@ -73,12 +73,12 @@ for i in {1..<mongos_number>}
 do
 	echo " Starting deploy of mongos${i}"
         deploy_vm "mem=<mongos_mem_mb> cpu-cores=<mongos_cpu_core>" machine_no
-		machine_mongos=$(juju status --format tabular | grep "^${machine_no} .*" | awk '{print $4}')
-		fqdn=$(juju status --format tabular | grep ${machine_mongos} | awk '{print $3}')
-        echo "mongos${i}: " >> /tmp/<env_name>-mongo-conf.yaml
-        echo "  mongos_port : <mongos_port>" >> /tmp/<env_name>-mongo-conf.yaml
-        echo "  machine: machine-${machine_no}" >> /tmp/<env_name>-mongo-conf.yaml
-        echo "  FQDN: ${fqdn} " >> /tmp/<env_name>-mongo-conf.yaml
+	machine_mongos=$(juju status --format tabular | grep "^${machine_no} .*" | awk '{print $4}')
+	fqdn=$(juju status --format tabular | grep ${machine_mongos} | awk '{print $3}')
+        echo "mongos${i}: " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+        echo "  mongos_port : <mongos_port>" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+        echo "  machine: machine-${machine_no}" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+        echo "  FQDN: ${fqdn} " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
 		juju deploy /root/.juju/charms/trusty/deploy-node "mongos${i}" --series trusty --to $machine_no 
 	
 		echo "Exposing mongos${1}"
@@ -98,23 +98,45 @@ do
 	deploy_vm "mem=<shard_mem_mb> cpu-cores=<shard_cpu_core>" machine_no
 	machine_shard=$(juju status --format tabular | grep "^${machine_no} .*" | awk '{print $4}')
 	fqdn=$(juju status --format tabular | grep ${machine_shard} | awk '{print $3}')
-	echo "shard${i}: " >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  shard_replica_set_name : <shard_repl_set_name>" >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  shard_port : <shard_port>" >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  machine: machine-${machine_no}" >> /tmp/<env_name>-mongo-conf.yaml
-	echo "  FQDN: ${fqdn} " >> /tmp/<env_name>-mongo-conf.yaml
+	echo "shard${i}: " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  shard_replica_set_name : <shard_repl_set_name>" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  shard_port : <shard_port>" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  machine: machine-${machine_no}" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+	echo "  FQDN: ${fqdn} " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
 	juju deploy /root/.juju/charms/trusty/deploy-node "shard${i}" --series trusty --to $machine_no 
 
 	echo "Exposing shard${i}"
 	juju expose "shard${i}"
 	sleep 30s
 	 echo " Starting chef add node : 'role[shard]' on host : ${fqdn}"	
-        knife bootstrap  ${fqdn} --ssh-user ubuntu --sudo -r 'role[shard]' --bootstrap-install-command 'curl -L https://www.chef.io/chef/install.sh | sudo bash' || { echo "Failed to bootstrap machine : ${machine_shard} role[shard]  "; exit 2; }
+        knife bootstrap  ${fqdn} --ssh-user ubuntu --sudo -r 'role[shard]' -j "{ \"mongodb3\" : { \"config\" : { \"mongod\" : {  \"replication\" : {  \"replSetName\" : \"<shard_repl_set_name>_shard${i}\" } } } } }" --bootstrap-install-command 'curl -L https://www.chef.io/chef/install.sh | sudo bash' || { echo "Failed to bootstrap machine : ${machine_shard} role[shard]  "; exit 2; }
 	echo " Successfully finished chef install 'role[shard]'  on host : ${fqdn}"
+	
+	for j in {1..<shard_repl_number>} 
+	do
+		echo " Starting deploy of shard${i}-replicaset${j}"
+		deploy_vm "mem=<shard_mem_mb> cpu-cores=<shard_cpu_core>" machine_no
+		machine_repl=$(juju status --format tabular | grep "^${machine_no} .*" | awk '{print $4}')
+		fqdn=$(juju status --format tabular | grep ${machine_repl} | awk '{print $3}')
+		echo "  shard-replicaset${j}: " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+ 		echo "    shard_replica_set_name : <shard_repl_set_name>_shard${i}" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+		echo "    shard_replicaset_port : <shard_port>" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+		echo "    machine: machine-${machine_no}" >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+		echo "    FQDN: ${fqdn} " >> /tmp/<cluster_name>-<env_name>-mongo-conf.yaml
+		juju deploy /root/.juju/charms/trusty/deploy-node "shard${i}-replicaset${j}" --series trusty --to $machine_no 
+
+		echo "Exposing  shard${i}-replicaset${j}"
+		juju expose "shard${i}-replicaset${j}"
+		sleep 30s
+		echo " Starting chef add node : 'role[replicaset]' on host : ${fqdn}"	
+		knife bootstrap  ${fqdn} --ssh-user ubuntu --sudo -r 'role[replicaset]' -j "{ \"mongodb3\" : { \"config\" : { \"mongod\" : {  \"replication\" : {  \"replSetName\" : \"<shard_repl_set_name>_shard${i}\" } } } } }" --bootstrap-install-command 'curl -L https://www.chef.io/chef/install.sh | sudo bash' || { echo "Failed to bootstrap machine : ${fqdn} role[replicaset]  "; exit 2; }
+
+		echo " Successfully finished chef install 'role[replicaset]'  on host : ${fqdn}"
+	done
 done
 
 
 echo "########################################################################"
-echo "# For deployed cluster info please see /tmp/<env_name>-mongo-conf.yaml "
+echo "# For deployed cluster info please see /tmp/<cluster_name>-<env_name>-mongo-conf.yaml "
 echo "# or run : juju status"
 echo "########################################################################"
