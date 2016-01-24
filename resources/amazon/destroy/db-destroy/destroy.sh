ed801fba96e427cd28d8709683cde5bd525bf804
Pash10g <pash10g@gmail.com>
2016-01-24 16:22:16 +0200

    Update destroy.sh

--- resources/amazon/destroy/db-destroy/destroy.sh
+++ resources/amazon/destroy/db-destroy/destroy.sh
@@ -3,6 +3,5 @@ set -e
 
 juju switch "<env_name>"  || { echo "ERROR While setting env <env_name> "; exit 2; }
 
-juju destroy-environment "<env_name>" -y || { echo "Using destroy-controller"; juju destroy-controller "<env_na
-me>" -y ; }
+juju destroy-environment "<env_name>" -y || { echo "Using destroy-controller"; juju destroy-controller "<env_name>" -y ; }
 
