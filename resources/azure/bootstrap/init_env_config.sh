dac02a1d315809f7d60be3c1943bf69d3c3050b8
Pash10g <pash10g@gmail.com>
2016-01-25 08:53:08 +0200

    Update init_env_config.sh

--- resources/azure/bootstrap/init_env_config.sh
+++ resources/azure/bootstrap/init_env_config.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
+set -e
+
 if ! which juju 2>/dev/null; then
-	sudo add-apt-repository ppa:juju/devel -y
+	echo "Installing juju..."
+	sudo add-apt-repository ppa:juju/stable -y
 	sudo apt-get update && sudo apt-get install juju-core -y
 fi
+
 if ! which knife 2>/dev/null; then
-        curl -L https://www.opscode.com/chef/install.sh | sudo bash
+	echo "Installing chef workstation..."
+	curl -L https://www.opscode.com/chef/install.sh | sudo bash
+fi
+ 
+
+
+#juju generate-config -f || { echo "ERROR Failed to create juju environments.yaml file" ; exit 2 }
+
+
+if [ -f /root/.juju/environments/<env_name>.json ]; then
+	rm  /root/.juju/environments/<env_name>.json
 fi
 
+mkdir -p /root/.chef/
 
 
+echo "Genrating juju configuration for env : '<env_name>'"
+juju generate-config -f 
 
-
-if ! which azure 2>/dev/null; then
-	sudo apt-get install nodejs-legacy -y
-	sudo apt-get install npm -y
-	sudo npm install -g azure-cli
-	echo "azure cli installed"
-fi
-azure config mode arm
-if ! azure account show 2>/dev/null; then
-	echo "Setting up first login"
-	azure login || { echo "ERROR ! Failed to login into azure account... "; exit 2; }
-fi	
-
-
-if [ -d /root/.juju/environments ]; then
-	rm -rf /root/.juju/environments/<env_name>*
-fi
-juju generate-config -f || { echo "Failed to generate init configuration "; exit 2; }
 echo "" > /root/.juju/environments.yaml
 
 
@@ -37,17 +36,10 @@ echo "environments:" >> /root/.juju/environments.yaml
 echo " " >> /root/.juju/environments.yaml
 echo "    <env_name>:" >> /root/.juju/environments.yaml
 echo "      type: azure" >> /root/.juju/environments.yaml
-echo "      application-id: <application_id>" >> /root/.juju/environments.yaml
-echo "      application-password: <application_password>" >> /root/.juju/environments.yaml
-echo "      subscription-id: <subscription_id>" >> /root/.juju/environments.yaml
-echo "      tenant-id: <tenant_id>" >> /root/.juju/environments.yaml
-echo "      location: <region>" >> /root/.juju/environments.yaml
+echo "      location:: <region>" >> /root/.juju/environments.yaml
+echo "      management-subscription-id: <subscription_id:>" >> /root/.juju/environments.yaml
+echo "      management-certificate-path: <management_certificate_path>" >> /root/.juju/environments.yaml
+echo "      storage-account-name: <storage_account_name>" >> /root/.juju/environments.yaml
 
 
-#
-#if [ -d /root/.juju/environments ]; then
-#	rm /root/.juju/environments/<env_name>*
-#fi
-#
-#echo "" > /root/.juju/environments.yaml
 
