d271d05a4ad53d4f9c20cb78e50da4784e7ec9b3
Pash10g <pash10g@gmail.com>
2016-01-26 11:56:42 +0200

    Update cp_chef.sh

--- resources/gce/deploy/db-create/chef/cp_chef.sh
+++ resources/gce/deploy/db-create/chef/cp_chef.sh
@@ -5,6 +5,9 @@ export BASE_DIR=`dirname $FULLPATH_SCRIPT`
 
 
 echo "Populate cookbooks and roles to /root/.chef/"
+if [ "<mms_api_key>" != "none" ]; then
+  sed -i "s#<mms-recipe>#,\"recipe[mongodb3::mms_monitoring_agent]\"#g" $BASE_DIR/*
+fi
 cp -R $BASE_DIR/cookbooks /root/.chef/ || { echo "ERROR Populate cookbooks "; exit 2;}
 cp -R $BASE_DIR/roles    /root/.chef/ || { echo "ERROR Populate cookbooks "; exit 2;}
 
@@ -12,3 +15,4 @@ cp -R $BASE_DIR/roles    /root/.chef/ || { echo "ERROR Populate cookbooks "; exi
 public_ip=$(juju status --format tabular | grep "machine-0" | awk '{print $5}')
 sed -i "s#<chef-server>#${public_ip}#g" $BASE_DIR/knife_conf/knife.rb 
 cp -R $BASE_DIR/knife_conf/knife.rb /root/.chef/
+
