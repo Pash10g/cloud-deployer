b83585a141f4a8d789f34e37e05eb85dfeda5cc6
Pash10g <pash10g@gmail.com>
2016-01-26 11:01:46 +0200

    Update cp_chef.sh

--- resources/gce/deploy/db-create/chef/cp_chef.sh
+++ resources/gce/deploy/db-create/chef/cp_chef.sh
@@ -8,6 +8,7 @@ echo "Populate cookbooks and roles to /root/.chef/"
 cp -R $BASE_DIR/cookbooks /root/.chef/ || { echo "ERROR Populate cookbooks "; exit 2;}
 cp -R $BASE_DIR/roles    /root/.chef/ || { echo "ERROR Populate cookbooks "; exit 2;}
 
-public_ip=$(juju status --format tabular | grep "machine-0" | awk '{print $4}')
+# Query the machine details of the chef-server
+public_ip=$(juju status --format tabular | grep "machine-0" | awk '{print $5}')
 sed -i "s#<chef-server>#${public_ip}#g" $BASE_DIR/knife_conf/knife.rb 
 cp -R $BASE_DIR/knife_conf/knife.rb /root/.chef/
