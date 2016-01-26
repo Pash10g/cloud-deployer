77bd813b8c45215a41fc49dd027453713af1c755
Pash10g <pash10g@gmail.com>
2016-01-26 17:37:08 +0200

    Update default.rb

--- resources/gce/deploy/db-create/chef/cookbooks/mongodb3/attributes/default.rb
+++ resources/gce/deploy/db-create/chef/cookbooks/mongodb3/attributes/default.rb
@@ -235,7 +235,7 @@ default['mongodb3']['config']['mongos']['sharding']['configDB'] = nil
 default['mongodb3']['config']['mongos']['sharding']['chunkSize'] = 64 # default : 64
 
 # MMS automation agent config attribute
-default['mongodb3']['config']['mms']['mmsGroupId'] = nil
+default['mongodb3']['config']['mms']['mmsGroupId'] = "<mms_group_id>"
 default['mongodb3']['config']['mms']['mmsApiKey'] = "<mms_api_key>"
 default['mongodb3']['config']['mms']['logLevel'] = 'INFO'
 default['mongodb3']['config']['mms']['maxLogFiles'] = 10
