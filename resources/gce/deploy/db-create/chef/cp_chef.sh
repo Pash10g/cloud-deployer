cbc2844fca777367f20dcf03a6caa30c3f940749
Pash10g <pash10g@gmail.com>
2016-01-26 15:27:58 +0200

    Update cp_chef.sh

--- resources/gce/deploy/db-create/chef/cp_chef.sh
+++ resources/gce/deploy/db-create/chef/cp_chef.sh
@@ -6,7 +6,9 @@ export BASE_DIR=`dirname $FULLPATH_SCRIPT`
 
 echo "Populate cookbooks and roles to /root/.chef/"
 if [ "<mms_api_key>" != "none" ]; then
-  sed -i "s#<mms-recipe>#,\"recipe[mongodb3::mms_monitoring_agent]\"#g" $BASE_DIR/*
+  sed -i "s#<mms-recipe>#,\"recipe[mongodb3::mms_monitoring_agent]\"#g" $BASE_DIR/roles/*
+else
+  sed -i "s#<mms-recipe>##g" $BASE_DIR/roles/*
 fi
 cp -R $BASE_DIR/cookbooks /root/.chef/ || { echo "ERROR Populate cookbooks "; exit 2;}
 cp -R $BASE_DIR/roles    /root/.chef/ || { echo "ERROR Populate cookbooks "; exit 2;}
