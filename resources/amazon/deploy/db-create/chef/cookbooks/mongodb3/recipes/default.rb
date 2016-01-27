6bd6282d635159a89acaea06875ea797fe879e81
Pash10g <pash10g@gmail.com>
2016-01-27 15:45:04 +0200

    Update default.rb

--- resources/amazon/deploy/db-create/chef/cookbooks/mongodb3/recipes/default.rb
+++ resources/amazon/deploy/db-create/chef/cookbooks/mongodb3/recipes/default.rb
@@ -61,24 +61,6 @@ execute "setup huge pages init.d" do
     		not_if "cat /sys/kernel/mm/transparent_hugepage/enabled | grep 'always madvise [never]' "
 end
 
-#Setup ulimits
-execute "setup ulimit changes -n" do
-    		command "sudo ulimit -n 64000"
-    		not_if "ulimit -n | grep '64000' "
-    		owner 'root'
-    		group 'root'
-    		notifies :restart, 'service[mongod]', :delayed
-end
-
-
-execute "setup ulimit changes -u" do
-    		command "sudo ulimit -u 64000"
-    		not_if "ulimit -u | grep '64000' "
-    		owner 'root'
-    		group 'root'
-    		notifies :restart, 'service[mongod]', :delayed
-end
-
 
 if node['mongodb3']['config']['mongod']['replication']['replSetName']  =~ /none/
   node.override['mongodb3']['config']['mongod']['replication']['replSetName'] = nil
